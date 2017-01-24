bd5f11194100f561c98e5078e9bc7bc375a92dd9
hjfitz <hfitz83@gmail.com>
2017-01-24 08:51:50 +0000

    it was a bracket all tihs time

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-  getWeather();
+  getWeather("dsn");
 });
--- public/javascripts/widgets/weather.js
+++ public/javascripts/widgets/weather.js
@@ -53,7 +53,7 @@ function getWeather(source="owm") {
           }
         }
       );
-    }
+    });
   }
 }
 
