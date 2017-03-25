5e171ae1d88dbac8215f5f17824c0cfae9bba3a4
Harry <hfitz83@gmail.com>
2017-03-25 16:12:00 +0000

    refactor

--- routes/index.js
+++ routes/index.js
@@ -4,54 +4,15 @@ const dbUtil = require('../util/db')
 const sqliteDB = dbUtil.db
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  sqliteDB.all('select * from todo', (err, todos) => {
-    if (err) console.error(err)
-    res.render('index-2', {
-      title: 'ws_cwk3',
-      'todos': todos
-    })
+  res.render('index', {
+    title: 'ws_cwk3'
   })
-})
-
-router.get('/drag', function (req, res, next) {
-  sqliteDB.all('select * from todo', (err, todos) => {
-    if (err) console.error(err)
-    res.render('index-dragdrop', {
-      title: 'ws_cwk3',
-      'todos': todos
-    })
-  })
-})
-
-router.get('/flow', function (req, res, next) {
-  sqliteDB.all('select * from todo', (err, todos) => {
-    if (err) console.error(err)
-    res.render('index-freeflow', {
-      title: 'ws_cwk3',
-      'todos': todos
-    })
-  })
-})
-
-router.get('/flow-dyncard', function (req, res, next) {
-  sqliteDB.all('select * from todo', (err, todos) => {
-    if (err) console.error(err)
-    res.render('index-freeflow-dyncard', {
-      title: 'ws_cwk3',
-      'todos': todos
-    })
-  })
-})
-
-router.get('/budget', function (req, res, next) {
-  res.render('budget', { title: 'budget' })
 })
 
 router.get('/todo', function (req, res, next) {
   res.render('../public/own-packages/newTodo/todoMin', {
     'title': 'To-do List Version 2',
-    'weather': 'class=hidden',
-    'todo': 'class=active'
+    'weather': 'class=hidden'
   })
 })
 
@@ -61,29 +22,11 @@ router.get('/config', function (req, res, next) {
   })
 })
 
-router.get('/news', function (req, res, next) {
-  res.render('news', {
-    'title': 'News',
-    'news': 'class=active'
-  })
-})
-
-router.get('/cal', function (req, res, next) {
-  res.render('gapi-auth', {
-    'title': 'gapi attempt'
-  })
-})
-
 router.get('/loc', function (req,res,next) {
     res.render('setLocation', {
         'title': 'location attempt'
     })
 })
 
-router.get('/test', function(req,res,next) {
-  res.render('index-rewrite', {
-    'title': 'test'
-  })
-})
 
 module.exports = router
